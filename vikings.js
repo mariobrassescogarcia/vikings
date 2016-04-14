95be64283f832be1ab3be90e18fa504af5fd54b2
Marito <mariobrassescogarcia@gmail.com>
2016-04-14 18:51:30 +0200

    Finished iteration 1

--- vikings.js
+++ vikings.js
@@ -12,24 +12,59 @@ Viking.prototype.hit = function (enemy) {
 
 
 
-var Ragnar = new Viking ("Ragnar", 100, 30);
-var Rolo = new Viking ("Rolo", 115, 25);
-var Lagarza = new Viking ("Lagarza", 80, 28);
-var Bjorn = new Viking ("Bjorn", 125, 20);
+var Ragnar = new Viking ("Ragnar", 1000, 130);
+var Rolo = new Viking ("Rolo", 1300, 115);
+var Lagarza = new Viking ("Lagarza", 800, 150);
+var Bjorn = new Viking ("Bjorn", 1200, 120);
 
 
-var PitFight = function(viking1, viking2, turns){
+
+
+var PitFight = function (viking1, viking2, turns){
+	
 	var turnnumber = 1;
-	while (viking1.health > 10 || viking2.health > 10 || turnnumber <= turns) {
-		viking1.hit(viking2);
-		viking2.hit(viking1);
-		console.log("End of turn " + turnnumber + ". " + viking1.name + "'s health: " + viking1.health + " ." + viking2.name + "'s health: " + viking2.health + ".");
-		turnnumber ++;
+
+
+	function finish_fight() {
+		console.log("The figth ends. " + viking1.name + " and " + viking2.name + " go to grab some beers together. ");
+		console.log("------------------------------------");
 	}
+
+	function should_attack(attacker, defender){
+		if (attacker.strength < defender.health){
+			return true;
+		}
+
+		else{
+			return false;
+		}	
+	}
+
+	function fight(){
+
+   while (turnnumber <= turns){
+		if (should_attack(viking1, viking2) === true){
+				viking1.hit(viking2);
+				console.log(viking1.name + " hits " + viking2.name + ". " + viking2.name + "'s health: " + viking2.health + ".");
+		}
+		else {
+			finish_fight()
+			break}
+
+		if (should_attack(viking2, viking1) === true){
+				viking2.hit(viking1);
+				console.log(viking2.name + " hits " + viking1.name + ". " + viking1.name + "'s health: " + viking1.health + ".");
+		}
+		else {
+			finish_fight()
+			break}		
 		
-};
+		turnnumber ++;
+   }
+	
+	}
+		fight();
+}
 
 
 brothersfight = new PitFight (Ragnar, Rolo, 10);
-
-console.log(Ragnar.health);
