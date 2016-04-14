1fa830c095f4f9cf093c4e0b00d7e66d38aab103
Marito <mariobrassescogarcia@gmail.com>
2016-04-14 17:44:37 +0200

    Created pitfight. Needs to stop the fight when life not > 0

--- vikings.js
+++ vikings.js
@@ -18,15 +18,18 @@ var Lagarza = new Viking ("Lagarza", 80, 28);
 var Bjorn = new Viking ("Bjorn", 125, 20);
 
 
-
-
-Ragnar.hit(Rolo);
-
-console.log(Rolo.health);
-
-
-
-
-var PitFight = function(viking1, viking2){
-
+var PitFight = function(viking1, viking2, turns){
+	var turnnumber = 1;
+	while (viking1.health > 10 || viking2.health > 10 || turnnumber <= turns) {
+		viking1.hit(viking2);
+		viking2.hit(viking1);
+		console.log("End of turn " + turnnumber + ". " + viking1.name + "'s health: " + viking1.health + " ." + viking2.name + "'s health: " + viking2.health + ".");
+		turnnumber ++;
+	}
+		
 };
+
+
+brothersfight = new PitFight (Ragnar, Rolo, 10);
+
+console.log(Ragnar.health);
